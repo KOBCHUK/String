c137306cf0aca7477ddeac4ca88592230644ca01
Eugen Kobchuk <irravadi@gmail.com>
2023-04-02 14:22:23 +0300

    lab_ex_4 done

--- String.py
+++ String.py
@@ -1,1 +1,16 @@
+try:
+    row = input("Enter the row: ")
+    search_word = input("Enter world: ")
+    row = row.lower()
+    count = 0
+    words = row.split()
 
+    for word in words:
+        if word == search_word:
+            count += 1
+
+    print("The world '{}' occurs {} items in the line.".format(search_word, count))
+except Exception as ex:
+    print(ex)
+finally:
+    print("exit")
