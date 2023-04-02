4e30ef66dcefab9a3efd403330022a08e1069738
Eugen Kobchuk <irravadi@gmail.com>
2023-04-02 10:43:07 +0300

    lab_ex_1 done

--- String.py
+++ String.py
@@ -1,3 +1,8 @@
-row = input("Enter a row: ")
-reversed_row = row[::-1]
-print("Reversed row:", reversed_row)
+try:
+    row = input("Enter a row: ")
+    reversed_row = row[::-1]
+    print("Reversed row:", reversed_row)
+except Exception as ex:
+    print(ex)
+finally:
+    print("Exit")
