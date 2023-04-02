026d21b46b3fb86549233676839b606442736e84
Eugen Kobchuk <irravadi@gmail.com>
2023-04-02 10:40:29 +0300

    lab_ex_2 done

--- String.py
+++ String.py
@@ -1,1 +1,17 @@
+try:
+    row = input("Enter the row: ")
+    letters = 0
+    numbers = 0
 
+    for char in row:
+        if char.isalpha():
+            letters += 1
+        elif char.isdigit():
+            numbers += 1
+
+    print("Letters:", letters)
+    print("Numbers:", numbers)
+except Exception as ex:
+    print(ex)
+finally:
+    print("Exit")
