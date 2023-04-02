39f75332c73eb3c8a31a33f93be769375445fc25
Eugen Kobchuk <irravadi@gmail.com>
2023-04-02 15:33:44 +0300

    lab_ex_4 done

--- String.py
+++ String.py
@@ -1,14 +1,18 @@
 try:
-    row = input("Enter the row: ")
-    world = input ("Enter the world: ")
-    row = row.lower()
-    row = row.split()
-   
-    count = row.count(world)
+    import re
+    text = input("Enter the row: ")
+    search_word = input("Enter the search world: ")
+    delimiters = [' ', ',', '.', ';', ':', '-', '?', '!', '(', ')', '\n']
+    count = 0
 
-    print (f"The world '{world}' occurs {count} times in the row")
+    words = re.split('|'.join(map(re.escape, delimiters)), text)
 
+    for word in words:
+        if word == search_word:
+            count += 1
+
+    print("The world \"{}\" occurs {} item in the line.".format(search_word, count))
 except Exception as ex:
     print(ex)
 finally:
-    ("Exit")
+    print("Exit")
