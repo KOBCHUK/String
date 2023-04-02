e828ac6b5371e0801b409c4b974a494b33a90f42
Eugen Kobchuk <irravadi@gmail.com>
2023-04-02 11:22:43 +0300

    lab_ex_4 done

--- String.py
+++ String.py
@@ -1,1 +1,11 @@
+try:
+    row = input("Enter the row: ")
+    world = input ("Enter the world: ")
+    
+    count = row.count(world)
 
+    print (f"The world '{world}' occurs {count} times in the row")
+except Exception as ex:
+    print(ex)
+finally:
+    ("Exit")
