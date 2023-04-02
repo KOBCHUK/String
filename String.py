a9c3a5c90129d509217048bc15727702edd730a3
Eugen Kobchuk <irravadi@gmail.com>
2023-04-02 11:29:50 +0300

    lab_ex_5 done

--- String.py
+++ String.py
@@ -1,1 +1,11 @@
+try:
+    row = input("Enter the row: ")
+    world_1 = input("Enter the world_1: ")
+    world_2 = input("Enter the world_2: ")
+    new_row = row.replace(world_1, world_2)
+    print(f"The resulting line is: {new_row}")
+except Exception as ex:
+    print(ex)
+finally:
+    print("exit")
 
