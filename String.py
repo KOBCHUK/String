194e49cd67f9d28f88e32e229e721caec7c491c6
Eugen Kobchuk <irravadi@gmail.com>
2023-04-02 14:38:07 +0300

    lab_ex_5_1 done

--- String.py
+++ String.py
@@ -1,1 +1,20 @@
+try:
+    row = input("Enter the row: ")
+    search_word = input("Enter the searth world: ")
+    replace_word = input("Enter the replace world: ")
+
+    words = row.split()
+    new_text = ""
+
+    for word in words:
+        if word == search_word:
+         new_text += replace_word + " "
+        else:
+         new_text += word + " "
+
+    print("Received row: ", new_text.strip())
+except Exception as ex:
+    print(ex)
+finally:
+    print("Exit")
 
