553737fb64b07fc61f434007093d37f75db0a048
Eugen Kobchuk <irravadi@gmail.com>
2023-04-02 10:56:19 +0300

    lab_ex_3 done

--- String.py
+++ String.py
@@ -1,1 +1,15 @@
+try:
+    row = input("Enter a row: ")
+    char = input("Enter a character: ")
+
+    row = row.lower()
+
+    count = row.count(char)
+
+    print(f"The character '{char}' occurs {count} times in the row.")
+
+except Exception as ex:
+    print(ex)
+finally:
+    print("exit")
 
